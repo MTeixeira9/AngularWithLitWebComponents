6812b03a454b1cd11b19f60599d5f38b565042e7
MTeixeira9 <mario_ze16@hotmail.com>
2022-03-24 17:14:04 +0000

    Added lit component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,38 @@
 import { Component } from '@angular/core';
+import { User } from 'src/app/web-components/card-user/user';
+import { CardUser } from './web-components/card-user/card-user';
 
 @Component({
   selector: 'corp-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'angular-lit-web-components';
+  users: User[] = [
+    {
+      id: 0,
+      fullName: 'Luis Avilles',
+      role: 'Software Engineer',
+      avatar: 'https://luixaviles.com/images/avatar@2x.png',
+    },
+    {
+      id: 1,
+      fullName: 'Roberto Perez',
+      role: 'UX Designer',
+    },
+    {
+      id: 2,
+      fullName: 'Maria Gomez',
+      role: 'Data Scientist',
+    },
+  ];
+
+  edit(event: Event) {
+    const user = (event as CustomEvent<User>).detail;
+    console.log('Edit user', user);
+  }
+
+  constructor() {
+    customElements.define('card-user', CardUser);
+  }
 }
